5435fc4a8c2edd47ef3d5355bcf9769446701bc4
Bram Verhulst <bram.verhulst@student.howest.be>
2024-05-10 21:06:47 +0200

    Add more clear boot screen

--- Info-Orbs/src/main.cpp
+++ Info-Orbs/src/main.cpp
@@ -29,7 +29,42 @@ GlobalTime* globalTime; // Initialize the global time
 String connectingString{ "" };
 
 Widget* widgets[COUNT_OF_WIDGETS];
-int8_t currentWidget = 0;
+int8_t currentWidget = 1;
+
+int connectionTimer{ 0 };
+const int connectionTimeout{ 10000 };
+bool isConnected{ true };
+String connectionStatus{ "" };
+
+void connectionTimedOut() {
+  switch(WiFi.status()){
+    case WL_CONNECTED:
+      connectingString = "Connected";
+      break;
+    case WL_NO_SSID_AVAIL:
+      connectingString = "No SSID available";
+      break;
+    case WL_CONNECT_FAILED:
+      connectingString = "Connection failed";
+      break;
+    case WL_IDLE_STATUS:
+      connectingString = "Idle status";
+      break;
+    case WL_DISCONNECTED:
+      connectingString = "Disconnected";
+      break;
+    default:
+      connectingString = "Unknown";
+      break;
+  }
+
+  Serial.println("Connection timed out");
+  TFT_eSPI& display = sm->getDisplay();
+  sm->selectScreen(0);
+  display.fillScreen(TFT_BLACK);
+  display.drawCentreString("Connection", 120, 80, 1);
+  display.drawCentreString(connectingString, 120, 100, 1);
+}
 
 void setup() {
 
@@ -40,41 +75,55 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Starting up...");
   Serial.println("Connecting to: " + String(WIFI_SSID));
-
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-
+  
   sm = new ScreenManager(tft);
   sm->selectAllScreens();
   sm->getDisplay().fillScreen(TFT_BLACK);
   sm->reset();
 
-  sm->selectScreen(0);
   TFT_eSPI& display = sm->getDisplay();
+
+  sm->selectScreen(0);
   display.fillScreen(TFT_BLACK);
   display.setTextSize(2);
   display.setTextColor(TFT_WHITE);
   display.drawCentreString("Connecting" + connectingString, 120, 80, 1);
 
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
   sm->selectScreen(1);
   display.fillScreen(TFT_BLACK);
-  display.setTextSize(2);
-  display.setTextColor(TFT_WHITE);
   display.drawCentreString("Connecting to", 120, 80, 1);
   display.drawCentreString("WiFi..", 120, 100, 1);
   display.drawCentreString(WIFI_SSID, 120, 130, 1);
 
   Serial.println("Connecting to WiFi..");
-  while (WiFi.status() != WL_CONNECTED) {
+  sm->selectScreen(0);
+  display.fillScreen(TFT_BLACK);
+  display.drawCentreString("Connecting", 120, 80, 1);
+  
+  while (WiFi.status() != WL_IDLE_STATUS && isConnected) {
     Serial.print(".");
     connectingString += ".";
     if(connectingString.length() > 3) {
       connectingString = "";
     }
-    sm->selectScreen(0);
-    display.fillScreen(TFT_BLACK);
-    display.drawCentreString("Connecting", 120, 80, 1);
+    
+    display.fillRect(0, 100, 240, 100, TFT_BLACK);
     display.drawCentreString(connectingString, 120, 100, 1);
+
+    connectionTimer += 500;
+    if(connectionTimer > connectionTimeout) {
+      isConnected = false;
+      break;
+    }
     delay(500);
+  }
+
+
+  if(!isConnected) {
+    connectionTimedOut();
+    return;
   }
 
   #ifdef GC9A01_DRIVER
@@ -105,17 +154,20 @@ void setup() {
 }
 
 void loop() {
-  globalTime->updateTime();
+  if(isConnected){
+    globalTime->updateTime();
 
-  if(buttonRight.pressed()) {
-    Serial.println("Right button pressed");
-    currentWidget++;
-    currentWidget %= COUNT_OF_WIDGETS;
-    sm->fillAllScreens(TFT_BLACK);
-    widgets[currentWidget]->setup();
+    // if(buttonRight.pressed()) {
+    //   Serial.println("Right button pressed");
+    //   currentWidget++;
+    //   currentWidget %= COUNT_OF_WIDGETS;
+    //   sm->fillAllScreens(TFT_BLACK);
+    //   widgets[currentWidget]->setup();
+    // }
+    // This is a BIG WIP
+
+    widgets[currentWidget]->update();
+    widgets[currentWidget]->draw();
+    delay(100);
   }
-
-  widgets[currentWidget]->update();
-  widgets[currentWidget]->draw();
-  delay(100);
 }
