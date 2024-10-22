b9a33d263471ff4bf76342ba010a61763800a768
Christian Erpelding <erpelding@ce-data.de>
2024-10-22 13:35:09 +0200

    Added support for LongPress

--- firmware/lib/button/Button.cpp
+++ firmware/lib/button/Button.cpp
@@ -4,28 +4,36 @@
 
 Button::Button(uint8_t pin)
     : m_pin(pin), m_delay(100), m_ignoreUntil(0),
+      m_stateLevel(RELEASED_LEVEL), m_state(STATE_NOTHING),
       m_hasChanged(false) {}
 
 void Button::begin() {
-#if BUTTON_MODE == INPUT_PULLDOWN
-    m_state = LOW;
-#else
-    m_state = HIGH;
-#endif
-    pinMode(m_pin, BUTTON_MODE);
+  pinMode(m_pin, BUTTON_MODE);
 }
 
-bool Button::read() {
+uint8_t Button::read() {
   if (m_ignoreUntil > millis()) {
-	return m_state;
+	  return m_state;
   }
 
-  if (digitalRead(m_pin) != m_state) {
+  if (digitalRead(m_pin) != m_stateLevel) {
     m_ignoreUntil = millis() + m_delay;
-    m_state = !m_state;
+    m_stateLevel = !m_stateLevel;
+    if (m_stateLevel == RELEASED_LEVEL) {
+      // Button was now released
+      // We now check if this was a long or a short press
+      if (millis() - m_pressedSince < LONGPRESS_TIME) {
+        m_state = STATE_PRESSED;
+      } else {
+        m_state = STATE_LONGPRESSED;
+      }
+    } else {
+      // Start button press
+      m_pressedSince = millis();
+      m_state = STATE_NOTHING;
+    }
     m_hasChanged = true;
   }
-
   return m_state;
 }
 
@@ -43,15 +51,22 @@ bool Button::has_changed() {
 }
 
 bool Button::pressed() {
-  if (read() == PRESSED && has_changed() == true)
+  if (read() == STATE_PRESSED && has_changed() == true) {
+    return true;
+  } else
+    return false;
+}
+
+bool Button::longPressed() {
+  if (read() == STATE_LONGPRESSED && has_changed() == true)
     return true;
   else
     return false;
 }
 
-bool Button::released() {
-  if (read() == RELEASED && has_changed() == true)
-    return true;
-  else
-    return false;
-}
+// bool Button::released() {
+//   if (read() == RELEASED && has_changed() == true)
+//     return true;
+//   else
+//     return false;
+// }
