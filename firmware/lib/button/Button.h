67e3ebc92be43834ec870fa5cd04f11475299e60
Christian Erpelding <erpelding@ce-data.de>
2024-10-22 13:35:09 +0200

    Added support for LongPress

--- firmware/lib/button/Button.h
+++ firmware/lib/button/Button.h
@@ -7,21 +7,35 @@ class Button
 	public:
 		Button(uint8_t pin);
 		void begin();
-		bool read();
+		uint8_t read();
 		bool toggled();
 		bool pressed();
-		bool released();
+		bool longPressed();
+		// bool released();
 		bool has_changed();
 		
-		const static bool PRESSED = LOW;
-		const static bool RELEASED = HIGH;
-	
+#if BUTTON_MODE == INPUT_PULLDOWN
+		const static bool PRESSED_LEVEL = HIGH;
+		const static bool RELEASED_LEVEL = LOW;
+#else
+		const static bool PRESSED_LEVEL = LOW;
+		const static bool RELEASED_LEVEL = HIGH;
+#endif
+
+		const static uint8_t STATE_NOTHING = 0;
+		const static uint8_t STATE_PRESSED = 1;
+		const static uint8_t STATE_LONGPRESSED = 2;
+
+		const static long LONGPRESS_TIME = 500;
+
 	private:
 		uint8_t  m_pin;
 		uint16_t m_delay;
-		bool     m_state;
+		bool     m_stateLevel;
+		uint8_t  m_state;
 		bool     m_hasChanged;
 		uint32_t m_ignoreUntil;
+		unsigned long m_pressedSince;
 };
 
 #endif
