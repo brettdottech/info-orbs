79bc8463ac3a29f659eea63a6cbad6518bcf4e12
HugoBalder <60902142+HugoBalder@users.noreply.github.com>
2024-10-30 08:19:38 +0100

    Removed comments and use english variable names

--- firmware/lib/globalTime/globalTime.cpp
+++ firmware/lib/globalTime/globalTime.cpp
@@ -40,12 +40,8 @@ void GlobalTime::updateTime() {
 
         m_day = day(m_unixEpoch);
         m_month = month(m_unixEpoch);
-//PSC 10/22/2024 Added Monthname in German
-//        m_monthName = monthStr(m_month);
         m_monthName = LOC_MONTH[m_month-1];
         m_year = year(m_unixEpoch);
-//PSC 10/22/2024 Added Weekday in German
-//        m_weekday = dayStr(weekday(m_unixEpoch));
         m_weekday = LOC_WEEKDAY[(weekday(m_unixEpoch))-1];
         m_time = String(m_hour) + ":" + (m_minute < 10 ? "0" + String(m_minute) : String(m_minute));
     }
