6eb8c10fd715bd4af3f51597e38c5fcc53383376
Christian Erpelding <erpelding@ce-data.de>
2024-11-21 23:06:46 +0100

    "Fill all" character for DSEGstended is # not ~

--- firmware/src/widgets/clockWidget.cpp
+++ firmware/src/widgets/clockWidget.cpp
@@ -133,8 +133,8 @@ void ClockWidget::displayDigit(int displayIndex, const String& lastDigit, const 
     if (shadowing) {
         m_manager.setFontColor(BG_COLOR, TFT_BLACK);
         if (CLOCK_FONT == DSEG14) {
-            // DSEG14 uses ~ to fill all segments
-            m_manager.drawString("~", defaultX, defaultY, fontSize, Align::MiddleCenter);
+            // DSEG14 (from DSEGstended) uses # to fill all segments
+            m_manager.drawString("#", defaultX, defaultY, fontSize, Align::MiddleCenter);
         } else if (CLOCK_FONT == DSEG7) {
             // DESG7 uses 8 to fill all segments
             m_manager.drawString("8", defaultX, defaultY, fontSize, Align::MiddleCenter);
