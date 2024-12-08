2b9398161306458e11549f4ed341a7482b8c9522
Christian Erpelding <erpelding@ce-data.de>
2024-12-08 02:38:38 +0100

    WeatherLocation and ScreenMode can be configured

--- firmware/src/widgets/weatherwidget/WeatherWidget.cpp
+++ firmware/src/widgets/weatherwidget/WeatherWidget.cpp
@@ -16,6 +16,10 @@
 #include "config_helper.h"
 
 WeatherWidget::WeatherWidget(ScreenManager &manager, ConfigManager &config) : Widget(manager, config) {
+    config.addConfigString("WeatherWidget", "weatherLocation", &m_weatherLocation, 40, "City/State for the weather");
+    String optModes[] = {"Light", "Dark"};
+    config.addConfigComboBox("WeatherWidget", "weatherScrMode", &m_screenMode, optModes, 2, "Weather Screen Mode");
+    Serial.printf("WeatherWidget initialized, loc=%s, mode=%d\n", m_weatherLocation.c_str(), m_screenMode);
     m_mode = MODE_HIGHS;
 }
 
@@ -37,9 +41,6 @@ void WeatherWidget::buttonPressed(uint8_t buttonId, ButtonState state) {
 
 void WeatherWidget::setup() {
     m_time = GlobalTime::getInstance();
-#ifdef WEATHER_SCREEN_MODE
-    m_screenMode = WEATHER_SCREEN_MODE;
-#endif
     configureColors();
 }
 
@@ -78,6 +79,10 @@ void WeatherWidget::update(bool force) {
 
 bool WeatherWidget::getWeatherData() {
     HTTPClient http;
+    String weatherUnits = m_weatherUnits == 0 ? "metric" : "us";
+    String httpRequestAddress = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
+                                String(m_weatherLocation.c_str()) + "/next3days?key=" + weatherApiKey + "&unitGroup=" + weatherUnits +
+                                "&include=days,current&iconSet=icons1&lang=" + LOC_LANG;
     http.begin(httpRequestAddress);
     int httpCode = http.GET();
     if (httpCode > 0) {
