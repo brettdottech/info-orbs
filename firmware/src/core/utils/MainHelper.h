46573df6e5e00755799f95055439940427916263
Christian Erpelding <erpelding@ce-data.de>
2024-12-09 04:48:55 +0100

    Added MainHelper to unclutter main.cpp

---
+++ firmware/src/core/utils/MainHelper.h
@@ -0,0 +1,45 @@
+#ifndef MAINHELPER_H
+#define MAINHELPER_H
+
+#include "Button.h"
+#include "ConfigManager.h"
+#include "ScreenManager.h"
+#include "TFT_eSPI.h"
+#include "WidgetSet.h"
+#include "icons.h"
+#include <Arduino.h>
+#include <WiFiManager.h>
+
+class MainHelper {
+public:
+    static void init(WiFiManager *wm, ConfigManager *cm, TFT_eSPI &tft, WidgetSet *ws);
+    static void isrButtonChangeLeft();
+    static void isrButtonChangeMiddle();
+    static void isrButtonChangeRight();
+
+    static void setupButtons();
+    static void setupConfig();
+
+    static void buttonPressed(uint8_t buttonId, ButtonState state);
+    static void checkButtons();
+    static void checkCycleWidgets();
+
+    static void setupWebPortalEndpoints();
+    static void handleEndpointButton();
+    static void handleEndpointButtons();
+
+    static void showWelcome(ScreenManager *screenManager);
+    static void resetCycleTimer();
+
+private:
+    static WiFiManager *s_wifiManager;
+    static ConfigManager *s_configManager;
+    static TFT_eSPI *s_tft;
+    static WidgetSet *s_widgetSet;
+
+    static int s_widgetCycleDelay;
+    static unsigned long s_widgetCycleDelayPrev;
+    static bool s_invertedOrbs;
+};
+
+#endif
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,6 +1,7 @@
 #include "Button.h"
 #include "ConfigManager.h"
 #include "GlobalTime.h"
+#include "MainHelper.h"
 #include "ScreenManager.h"
 #include "Utils.h"
 #include "WidgetSet.h"
@@ -24,99 +25,16 @@
 
 TFT_eSPI tft = TFT_eSPI();
 
-#ifdef WIDGET_CYCLE_DELAY
-int m_widgetCycleDelay = WIDGET_CYCLE_DELAY; // Automatically cycle widgets every X seconds, set to 0 to disable
-#else
-int m_widgetCycleDelay = 0;
-#endif
-unsigned long m_widgetCycleDelayPrev = 0;
-bool m_invertedOrbs = INVERTED_ORBS;
-
-Button buttonLeft(BUTTON_LEFT);
-Button buttonOK(BUTTON_OK);
-Button buttonRight(BUTTON_RIGHT);
-
 GlobalTime *globalTime; // Initialize the global time
 
 String connectingString{""};
 
 WifiWidget *wifiWidget{nullptr};
 
-int connectionTimer{0};
-const int connectionTimeout{10000};
-bool isConnected{true};
-
-ScreenManager *sm;
-ConfigManager *config;
-WiFiManager *wifiManager;
-WidgetSet *widgetSet;
-
-/**
- * The ISR handlers must be static
- */
-void isrButtonChangeLeft() { buttonLeft.isrButtonChange(); }
-void isrButtonChangeMiddle() { buttonOK.isrButtonChange(); }
-void isrButtonChangeRight() { buttonRight.isrButtonChange(); }
-
-void setupButtons() {
-    buttonLeft.begin();
-    buttonOK.begin();
-    buttonRight.begin();
-
-    attachInterrupt(digitalPinToInterrupt(BUTTON_LEFT), isrButtonChangeLeft, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(BUTTON_OK), isrButtonChangeMiddle, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(BUTTON_RIGHT), isrButtonChangeRight, CHANGE);
-}
-
-void setupConfig() {
-    config->addConfigInt("General", "widgetCycDelay", &m_widgetCycleDelay, "Automatically cycle widgets every X seconds, set to 0 to disable");
-    config->addConfigBool("General", "invertedOrbs", &m_invertedOrbs, "Inverted Orbs (enable if using InfoOrbs upside down)");
-    tft.setRotation(m_invertedOrbs ? 2 : 0);
-}
-
-// Forward declaration
-void buttonPressed(uint8_t buttonId, ButtonState state);
-
-// Handle simulated button state
-void handleEndpointButton() {
-    if (wifiManager->server->hasArg("name") && wifiManager->server->hasArg("state")) {
-        String inButton = wifiManager->server->arg("name");
-        String inState = wifiManager->server->arg("state");
-        uint8_t buttonId = Button::stringToButtonId(inButton);
-        ButtonState state = Button::stringToButtonState(inState);
-
-        if (buttonId != 0 && state != BTN_NOTHING) {
-            buttonPressed(buttonId, state);
-            wifiManager->server->send(200, "text/plain", "OK " + inButton + "/" + inState + " -> " + String(buttonId) + "/" + String(state));
-            return;
-        }
-    }
-    wifiManager->server->send(500, "text/plain", "ERR");
-}
-
-// Show button web page
-void handleEndpointButtons() {
-    String msg = "<html><body style='background: black';><table>";
-    String buttons[] = {"left", "middle", "right"};
-    String states[] = {"short", "medium", "long"};
-    int numButtons = 3; // number of buttons
-    int numStates = 3; // number of states
-    for (int s = 0; s < numStates; s++) {
-        msg += "<tr>";
-        for (int b = 0; b < numButtons; b++) {
-            msg += "<td style='padding: 10px;'><button style='height: 50px; width: 150px;' onclick=\"sendReq('" + buttons[b] + "', '" + states[s] + "')\">" + buttons[b] + " " + states[s] + "</button></td>";
-        }
-        msg += "</tr>";
-    }
-    msg += "</table><script>function sendReq(name, state) {fetch(`/button?name=${name}&state=${state}`);}</script></body></html>";
-    wifiManager->server->send(200, "text/html", msg);
-}
-
-void setupWebPortalEndpoints() {
-    // To simulate button presses call e.g. http://<ip>/button?name=right&state=short
-    wifiManager->server->on("/button", handleEndpointButton);
-    wifiManager->server->on("/buttons", handleEndpointButtons);
-}
+ScreenManager *sm{nullptr};
+ConfigManager *config{nullptr};
+WiFiManager *wifiManager{nullptr};
+WidgetSet *widgetSet{nullptr};
 
 void setup() {
     Serial.begin(115200);
@@ -125,36 +43,16 @@ void setup() {
 
     wifiManager = new WiFiManager();
     config = new ConfigManager(*wifiManager);
-
-    setupConfig();
-    setupButtons();
-
     sm = new ScreenManager(tft);
-    sm->fillAllScreens(TFT_BLACK);
-    sm->setFontColor(TFT_WHITE);
-
-    sm->selectScreen(0);
-    sm->drawCentreString("Welcome", ScreenCenterX, ScreenCenterY, 29);
-
-    sm->selectScreen(1);
-    sm->drawCentreString("Info Orbs", ScreenCenterX, ScreenCenterY - 50, 22);
-    sm->drawCentreString("by", ScreenCenterX, ScreenCenterY - 5, 22);
-    sm->drawCentreString("brett.tech", ScreenCenterX, ScreenCenterY + 30, 22);
-    sm->setFontColor(TFT_RED);
-    sm->drawCentreString("version: 1.1.0", ScreenCenterX, ScreenCenterY + 65, 14);
-
-    sm->selectScreen(2);
-
-    sm->drawJpg(0, 0, logo_start, logo_end - logo_start);
-
     widgetSet = new WidgetSet(sm, *config);
 
-#ifdef GC9A01_DRIVER
-    Serial.println("GC9A01 Driver");
-#endif
-#if HARDWARE == WOKWI
-    Serial.println("Wokwi Build");
-#endif
+    // Pass references to MainHelper
+    MainHelper::init(wifiManager, config, tft, widgetSet);
+
+    MainHelper::setupConfig();
+    MainHelper::setupButtons();
+
+    MainHelper::showWelcome(sm);
 
     pinMode(BUSY_PIN, OUTPUT);
     Serial.println("Connecting to WiFi");
@@ -184,63 +82,7 @@ void setup() {
 
     config->setupWiFiManager();
 
-    m_widgetCycleDelayPrev = millis();
-}
-
-void checkCycleWidgets() {
-    if (m_widgetCycleDelay > 0 && (m_widgetCycleDelayPrev == 0 || (millis() - m_widgetCycleDelayPrev) >= m_widgetCycleDelay * 1000)) {
-        widgetSet->next();
-        m_widgetCycleDelayPrev = millis();
-    }
-}
-
-void buttonPressed(uint8_t buttonId, ButtonState state) {
-    if (state == BTN_NOTHING) {
-        // nothing to do
-        return;
-    }
-    // Reset cycle timer whenever a button is pressed
-    if (buttonId == BUTTON_LEFT && state == BTN_SHORT) {
-        // Left short press cycles widgets backward
-        Serial.println("Left button short pressed -> switch to prev Widget");
-        m_widgetCycleDelayPrev = millis();
-        widgetSet->prev();
-    } else if (buttonId == BUTTON_RIGHT && state == BTN_SHORT) {
-        // Right short press cycles widgets forward
-        Serial.println("Right button short pressed -> switch to next Widget");
-        m_widgetCycleDelayPrev = millis();
-        widgetSet->next();
-    } else {
-        // Everying else that is not BTN_NOTHING will be forwarded to the current widget
-        if (buttonId == BUTTON_LEFT) {
-            Serial.printf("Left button pressed, state=%d\n", state);
-            m_widgetCycleDelayPrev = millis();
-            widgetSet->buttonPressed(BUTTON_LEFT, state);
-        } else if (buttonId == BUTTON_OK) {
-            Serial.printf("Middle button pressed, state=%d\n", state);
-            m_widgetCycleDelayPrev = millis();
-            widgetSet->buttonPressed(BUTTON_OK, state);
-        } else if (buttonId == BUTTON_RIGHT) {
-            Serial.printf("Right button pressed, state=%d\n", state);
-            m_widgetCycleDelayPrev = millis();
-            widgetSet->buttonPressed(BUTTON_RIGHT, state);
-        }
-    }
-}
-
-void checkButtons() {
-    ButtonState leftState = buttonLeft.getState();
-    if (leftState != BTN_NOTHING) {
-        buttonPressed(BUTTON_LEFT, leftState);
-    }
-    ButtonState middleState = buttonOK.getState();
-    if (middleState != BTN_NOTHING) {
-        buttonPressed(BUTTON_OK, middleState);
-    }
-    ButtonState rightState = buttonRight.getState();
-    if (rightState != BTN_NOTHING) {
-        buttonPressed(BUTTON_RIGHT, rightState);
-    }
+    MainHelper::resetCycleTimer();
 }
 
 void loop() {
@@ -252,17 +94,17 @@ void loop() {
     } else {
         if (!widgetSet->initialUpdateDone()) {
             widgetSet->initializeAllWidgetsData();
-            setupWebPortalEndpoints();
+            MainHelper::setupWebPortalEndpoints();
         }
         globalTime->updateTime();
 
-        checkButtons();
+        MainHelper::checkButtons();
 
         widgetSet->updateCurrent();
         widgetSet->updateBrightnessByTime(globalTime->getHour24());
         widgetSet->drawCurrent();
 
-        checkCycleWidgets();
+        MainHelper::checkCycleWidgets();
 
         wifiWidget->processWebPortalRequests();
     }
