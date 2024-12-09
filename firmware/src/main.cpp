b285c4aa98cd3e01d690e47bc2924426e2b34f49
brett-dot-tech <brett-dot-tech@users.noreply.github.com>
2024-12-09 06:13:52 +0000

    [MegaLinter] Apply linters fixes

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -99,7 +99,6 @@ void setup() {
     sm->setFontColor(TFT_RED);
     sm->drawCentreString("version: 1.1.0", ScreenCenterX, ScreenCenterY + 65, 14);
 
-
     sm->selectScreen(2);
 
     TJpgDec.setJpgScale(1);
