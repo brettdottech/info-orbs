2b632a3d70f8851abfad4ef78d12d4312ddc7a31
ScottWell1 <19198944+ScottWell1@users.noreply.github.com>
2024-12-21 17:21:54 -0800

    Update ClockWidget.cpp
    Fix location of clock seconds indicator to be centered (rather than started) at correct angle.

--- firmware/src/widgets/clockwidget/ClockWidget.cpp
+++ firmware/src/widgets/clockwidget/ClockWidget.cpp
@@ -284,9 +284,11 @@ void ClockWidget::displaySeconds(int displayIndex, int seconds, int color) {
         color = m_config.getConfigInt(tickColorKey.c_str(), TFT_WHITE);
     }
     m_manager.selectScreen(displayIndex);
-    int startA = ((seconds * 6) + 180 - 3) % 360;
-    int endA = ((seconds * 6) + 180 + 3) % 360;
-    m_manager.drawSmoothArc(SCREEN_SIZE / 2, SCREEN_SIZE / 2, 120, 110, startA, endA, color, TFT_BLACK);
+    int startA = (seconds * 6) + 180 - 3;
+    int endA = (seconds * 6) + 180 + 3;
+    if (startA > 360) {startA = startA - 360;}
+    if (endA > 360 ) {endA = endA - 360;}
+    m_manager.drawSmoothArc(SCREEN_SIZE / 2, SCREEN_SIZE / 2, 120, 100, startA, endA, color, TFT_BLACK);
 }
 
 void ClockWidget::displayDigitImage(int displayIndex, const String &digit) {
