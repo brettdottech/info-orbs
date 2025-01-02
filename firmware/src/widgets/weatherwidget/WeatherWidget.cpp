4121d14e2f889e1a7bb3fb65b651eb2cc8f67f4f
flattermann <flattermann@users.noreply.github.com>
2025-01-02 20:48:53 +0000

    [MegaLinter] Apply linters fixes

--- firmware/src/widgets/weatherwidget/WeatherWidget.cpp
+++ firmware/src/widgets/weatherwidget/WeatherWidget.cpp
@@ -13,7 +13,6 @@
 #include "icons.h"
 #include <ArduinoJson.h>
 
-
 WeatherWidget::WeatherWidget(ScreenManager &manager, ConfigManager &config) : Widget(manager, config) {
     m_enabled = true; // Enabled by default
     m_config.addConfigBool("WeatherWidget", "weatherEnabled", &m_enabled, "Enable Widget");
@@ -41,7 +40,7 @@ void WeatherWidget::buttonPressed(uint8_t buttonId, ButtonState state) {
     if (buttonId == BUTTON_OK && state == BTN_SHORT)
         changeMode();
     if (buttonId == BUTTON_OK && state == BTN_MEDIUM)
-        update(true);        
+        update(true);
 }
 
 void WeatherWidget::setup() {
@@ -83,19 +82,20 @@ void WeatherWidget::update(bool force) {
 bool WeatherWidget::getWeatherData() {
     String weatherUnits = m_weatherUnits == 0 ? "metric" : "us";
     String httpRequestAddress = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
-                               String(m_weatherLocation.c_str()) + "/next3days?key=" + weatherApiKey + "&unitGroup=" + weatherUnits +
-                               "&include=days,current&iconSet=icons1&lang=" + LOC_LANG;
+                                String(m_weatherLocation.c_str()) + "/next3days?key=" + weatherApiKey + "&unitGroup=" + weatherUnits +
+                                "&include=days,current&iconSet=icons1&lang=" + LOC_LANG;
 
-    return HTTPClientWrapper::getInstance()->addRequest(httpRequestAddress,
-        [this](int httpCode, const String& response) {
+    return HTTPClientWrapper::getInstance()->addRequest(
+        httpRequestAddress,
+        [this](int httpCode, const String &response) {
             processResponse(httpCode, response);
         },
-        [this](int httpCode, String& response) {
+        [this](int httpCode, String &response) {
             preProcessResponse(httpCode, response);
         });
 }
 
-void WeatherWidget::preProcessResponse(int httpCode, String& response) {
+void WeatherWidget::preProcessResponse(int httpCode, String &response) {
     if (httpCode > 0) {
         JsonDocument filter;
         filter["resolvedAddress"] = true;
@@ -118,7 +118,7 @@ void WeatherWidget::preProcessResponse(int httpCode, String& response) {
     }
 }
 
-void WeatherWidget::processResponse(int httpCode, const String& response) {
+void WeatherWidget::processResponse(int httpCode, const String &response) {
     if (httpCode > 0) {
         JsonDocument doc;
         DeserializationError error = deserializeJson(doc, response);
