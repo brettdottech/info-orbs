2ebcd941eff496aecc63b1fc50e5f5b2e5d1fe4d
David Reed <212698+dreed47@users.noreply.github.com>
2025-01-06 11:05:05 -0500

    update approach slightly

--- firmware/src/core/utils/ShowMemoryUsage.cpp
+++ firmware/src/core/utils/ShowMemoryUsage.cpp
@@ -6,8 +6,9 @@ static unsigned long s_lastMemoryUsageShownAt = 0;
 
 void ShowMemoryUsage::printSerial(bool force, bool newLine) {
     multi_heap_info_t info;
-    if (force || millis() - s_lastMemoryUsageShownAt >= (MEMORY_DEBUG_INTERVAL ? MEMORY_DEBUG_INTERVAL : 1000)) {
-        heap_caps_get_info(&info, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT); // internal RAM, memory capable to store data or to create new task
+    const unsigned long interval = MEMORY_DEBUG_INTERVAL ? MEMORY_DEBUG_INTERVAL : 1000;
+    if (force || (!s_lastMemoryUsageShownAt || (millis() - s_lastMemoryUsageShownAt >= interval))) {
+        heap_caps_get_info(&info, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
         size_t total = heap_caps_get_total_size(MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
         Serial.printf("total: %d, allocated: %d, totalFree: %d, minFree: %d, largestFree: %d%s", total, info.total_allocated_bytes, info.total_free_bytes, info.minimum_free_bytes, info.largest_free_block, newLine ? "\n" : "");
         s_lastMemoryUsageShownAt = millis();
