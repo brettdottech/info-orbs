a9d602942d221785ddd34b2c0538f786ac9871c0
Christian Erpelding <erpelding@ce-data.de>
2025-02-24 04:16:53 +0100

    Enhance embedding logic to support wildcard file matching
    Updated the scripts to include wildcard file matching using `glob` for greater flexibility in file selection. Refactored and fixed formatting inconsistencies for improved readability. These changes streamline file embedding and copying processes in build workflows.

--- scripts/copy_files_to_littlefs.py
+++ scripts/copy_files_to_littlefs.py
@@ -6,7 +6,7 @@
 # You do NOT need to run it manually
 ###########################################################################################################
 
-import re, shutil, os, os.path
+import re, shutil, os, os.path, glob
 from SCons.Script import Import
 
 # Extract macros from the config header file
@@ -18,66 +18,68 @@ clear_out_dir = True
 # Map macros to directories and their respective files
 # The final filename is constructed as "dir + file" so the dir should end with a "/" unless you know what you are doing
 embed_map = {
-    "CONFIG_H != 0": [ # Always true
-        ["i18n/", # Source directory
-            ["en.txt"], # Files
-            0], # Do not skip parent directories
+    "CONFIG_H != 0": [  # Always true
+        ["i18n/",  # Source directory
+         ["*.txt"],  # Files
+         0],  # Do not skip parent directories
     ],
-    "USE_CLOCK_CUSTOM > 0": [ # Condition
-        ["images/clock/CustomClock0/", # Source directory
-            ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"], # Files
-            2], # Skip first two levels while copying ("images/clock/")
+    "USE_CLOCK_CUSTOM > 0": [  # Condition
+        ["images/clock/CustomClock0/",  # Source directory
+         ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
+         # Files
+         2],  # Skip first two levels while copying ("images/clock/")
     ],
     "USE_CLOCK_CUSTOM > 1": [
-        ["images/clock/CustomClock1/", 
-            ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
-            2],
+        ["images/clock/CustomClock1/",
+         ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
+         2],
     ],
     "USE_CLOCK_CUSTOM > 2": [
-        ["images/clock/CustomClock2/", 
-            ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
-            2],
+        ["images/clock/CustomClock2/",
+         ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
+         2],
     ],
     "USE_CLOCK_CUSTOM > 3": [
-        ["images/clock/CustomClock3/", 
-            ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
-            2],
+        ["images/clock/CustomClock3/",
+         ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
+         2],
     ],
     "USE_CLOCK_CUSTOM > 4": [
-        ["images/clock/CustomClock4/", 
-            ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
-            2],
+        ["images/clock/CustomClock4/",
+         ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
+         2],
     ],
     "USE_CLOCK_CUSTOM > 5": [
-        ["images/clock/CustomClock5/", 
-            ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
-            2],
+        ["images/clock/CustomClock5/",
+         ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
+         2],
     ],
     "USE_CLOCK_CUSTOM > 6": [
-        ["images/clock/CustomClock6/", 
-            ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
-            2],
+        ["images/clock/CustomClock6/",
+         ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
+         2],
     ],
     "USE_CLOCK_CUSTOM > 7": [
-        ["images/clock/CustomClock7/", 
-            ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
-            2],
+        ["images/clock/CustomClock7/",
+         ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
+         2],
     ],
     "USE_CLOCK_CUSTOM > 8": [
-        ["images/clock/CustomClock8/", 
-            ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
-            2],
+        ["images/clock/CustomClock8/",
+         ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
+         2],
     ],
     "USE_CLOCK_CUSTOM > 9": [
-        ["images/clock/CustomClock9/", 
-            ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
-            2],
+        ["images/clock/CustomClock9/",
+         ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg"],
+         2],
     ],
 }
 
 Import("env")
 
 board = env.BoardConfig()
+
 
 # Function to extract macros and their values from a header file
 def extract_macros_with_values(file_path):
@@ -109,12 +111,14 @@ def extract_macros_with_values(file_path):
         print(f"Warning: Header file '{file_path}' not found.")
     return macros
 
+
 def copy_files_to_builddir(files):
     for srcFile, dstFile in files:
         base = os.path.dirname(dstFile)
         finalPath = os.path.join(out_dir, base)
         os.makedirs(finalPath, exist_ok=True)
         shutil.copy(srcFile, finalPath)
+
 
 def clear_directory(directory):
     if os.path.exists(directory) and os.path.isdir(directory):
@@ -125,7 +129,8 @@ def clear_directory(directory):
             elif os.path.isdir(item_path):
                 shutil.rmtree(item_path)  # Remove directory and its contents
     else:
-        print(f"The path {directory} does not exist or is not a directory.")    
+        print(f"The path {directory} does not exist or is not a directory.")
+
 
 def action():
     # Clear LittleFS directory first
@@ -134,13 +139,15 @@ def action():
         clear_directory(out_dir)
     # Extract -D flags from BUILD_FLAGS
     build_flags = env.get("BUILD_FLAGS", [])
-    cpp_defines = {flag[2:].split("=")[0].strip(): flag[2:].split("=")[1].strip() if "=" in flag else None for flag in build_flags if flag.startswith("-D")}
+    cpp_defines = {flag[2:].split("=")[0].strip(): flag[2:].split("=")[1].strip() if "=" in flag else None for flag in
+                   build_flags if flag.startswith("-D")}
 
     # Extract macros from the config header file
     header_macros = extract_macros_with_values(config_header_path)
 
     # Combine all macros
     all_macros = {**header_macros, **cpp_defines}
+
     # Uncomment for debugging
     # print("Extracted macros with values:", all_macros)
 
@@ -166,8 +173,12 @@ def action():
                 # Skip some levels if necessary
                 parts = directory.split("/")
                 final_directory = "/".join(parts[skip_level:])
+                matched_files = []
+                for file in files:
+                    matched_files.extend(glob.glob(os.path.join(directory, file)))
                 # Add [src, dst] to out list
-                to_copy.extend([[directory + file, final_directory + file] for file in files])
+                to_copy.extend(
+                    [[mfile, os.path.join(final_directory, os.path.basename(mfile))] for mfile in matched_files])
 
     # Update the build environment with embedded files
     if len(to_copy) > 0:
@@ -175,6 +186,7 @@ def action():
         copy_files_to_builddir(to_copy)
     else:
         print("No files selected for embedding.")
+
 
 # I would prefer to use
 # env.AddPreAction("buildprog", action)
