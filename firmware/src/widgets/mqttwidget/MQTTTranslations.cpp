e0938f9012a77101b0973e9988bcec84b05d49b7
Christian Erpelding <erpelding@ce-data.de>
2025-02-28 19:10:16 +0100

    Add i18n support for MQTTWidget configuration fields
    Integrated internationalization (i18n) by introducing `MQTTTranslations.h` and `MQTTTranslations.cpp`. Updated the MQTTWidget configuration fields to use translatable strings for better localization support. This enhances usability for non-English users.

---
+++ firmware/src/widgets/mqttwidget/MQTTTranslations.cpp
@@ -0,0 +1,26 @@
+#include "MQTTTranslations.h"
+
+// Languages are defined in I18n.h:
+// EN, DE, FR
+
+// All translation variables for MQTTWidget should start with "t_mqtt"
+
+constexpr Translation t_mqttHost = {
+    "MQTT Host", // EN
+};
+
+constexpr Translation t_mqttPort = {
+    "MQTT Port", // EN
+};
+
+constexpr Translation t_mqttSetupTopic = {
+    "MQTT Setup Topic", // EN
+};
+
+constexpr Translation t_mqttUser = {
+    "MQTT user (empty if authentication is not required)", // EN
+};
+
+constexpr Translation t_mqttPass = {
+    "MQTT pass (empty if authentication is not required)", // EN
+};
